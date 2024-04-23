90fe42daa092fcca955f0990646fc10b75b50323
Dentro2000 <kowalski.szymon@outlook.com>
2024-04-23 21:55:34 +0200

    delete not needed code

--- cd_collection/cd_collection.infrastructure/DataAccessLayer/Configurations/CollectionsConfiguration.cs
+++ cd_collection/cd_collection.infrastructure/DataAccessLayer/Configurations/CollectionsConfiguration.cs
@@ -22,9 +22,6 @@ internal sealed class CollectionsConfiguration : IEntityTypeConfiguration<Collec
 
         builder.Property(x => x.LastUpdate)
             .HasConversion(x => x.Value, x => new Date(x));
-
-        // builder.Property(x => x.ItemIdentifiers)
-        //     .HasPostgresArrayConversion(x => x.Value, x => new CdItemId(x));
-
+        
     }
 }
